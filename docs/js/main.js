99e77fc00d70d695ac81c5170bebb4fc0f112857
v.eremenko <v.eremenko@allmonitoring.ru>
2020-12-14 22:23:00 +0300

    Project Unit

---
+++ docs/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        navSpeed: 1000,
+        dots: false,
+        loop: true,
+        navText: ('', '')
+    });
+  });
